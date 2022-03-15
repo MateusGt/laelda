669341aaf8c9f0c4f607fd31c7f561b19e4cb3d2
Julian David Mesa <julian98mesa@gmail.com>
2022-03-14 23:45:32 -0500

    La Elda 0.1

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// const idSecciones = document.querySelector('#v-pills-inventario-tab');
+
+
+// idSecciones.addEventListener('click', () => {
+
+//     if (idSecciones) {
+//         document.getElementById('v-pills-inventario-tab').style.backgroundColor = 'red';
+//     }else{
+//         idSecciones.removeEventListener(idSecciones)
+//     }
+
+
+
+//     // document.getElementById('v-pills-inventario-tab').style.color = '#090c15';
+
+// })
+
